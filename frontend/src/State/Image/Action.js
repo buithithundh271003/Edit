99d32ca18a5906eb35d5dbb3e64cdff08a485a82
Thu Bui <buithithundh2003@gmail.com>
2025-05-09 13:46:47 +0700

    2

--- frontend/src/State/Image/Action.js
+++ frontend/src/State/Image/Action.js
@@ -1,5 +1,6 @@
 import { api } from "../../config/apiConfig";
 import { FIND_IMAGE_FAILURE, FIND_IMAGE_REQUEST, FIND_IMAGE_SUCCESS,CREATE_IMAGE_FAILURE, CREATE_IMAGE_REQUEST, CREATE_IMAGE_SUCCESS, GET_IMAGE_FAILURE, GET_IMAGE_REQUEST, GET_IMAGE_SUCCESS , DELETE_IMAGE_FAILURE, DELETE_IMAGE_REQUEST, DELETE_IMAGE_SUCCESS} from "./ActionType"
+import { API_BASE_URL } from "../../config/apiConfig";
 
 export const createImage = (reqData) => async(dispatch)=>{
     dispatch({type: CREATE_IMAGE_REQUEST});
